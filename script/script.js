cc43b503e16fc5c6e728b4ddaf7b37faf4eef689
sma3797 <sma3797@gmail.com>
2020-04-02 19:06:02 +0500

    reset func added

--- script/script.js
+++ script/script.js
@@ -2,51 +2,57 @@ let queryForm = document.getElementsByClassName("queryForm");
 let input = document.getElementsByClassName("input");
 let form = document.getElementById("form");
 let selectEl = 0;
-let i = 0;
+const thankyou = document.querySelector(".thankyou");
+let textarea = document.getElementById("textarea");
+let submit = document.querySelectorAll(".submit");
+
 let reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+	thankyou.classList.remove("display");
+	queryForm[0].classList.remove("height");
+	queryForm[1].classList.remove("height");
+	submit.forEach((item) => {
+		item.disabled = false;
+	});
+	for (let key in input) {
+		textarea.disabled = false;
+		textarea.value = "";
+		input[key].disabled = false;
+	}
+	for (let inde in input) {
+		input[inde].style.removeProperty("transform");
+		input[inde].classList.remove("gray");
+	}
+});
 
-// function resetFunc() {}
+submit.forEach((item) => {
+	item.addEventListener("click", (event) => {
+		if (!item.lastElementChild) {
+			event.preventDefault();
+			item.disabled = true;
+			thankyou.classList.add("display");
+			for (let key in input) {
+				input[key].disabled = true;
+			}
+			textarea.disabled = true;
+			console.log(selectEl, ": on line 38");
+			console.log(textarea.value, ": on line 39");
+		}
+		if (item.lastElementChild) {
+			item.disabled = true;
+			thankyou.classList.add("display");
+			for (let key in input) {
+				input[key].disabled = true;
+			}
+			console.log(selectEl, ": on line 38");
+			console.log(textarea.value, ": on line 39");
+			item.lastElementChild.href.click();
+		}
+	});
+});
 
-// document.getElementById("reset").addEventListener("click", resetFunc);
-// // reset.addEventListener("click", resetFunc());
-
-// function onSubmit(event) {
-// 	event.preventDefault();
-// 	selectEl = event.explicitOriginalTarget.value;
-// 	if (selectEl <= 8) {
-// 		queryForm[1].classList.remove("height");
-// 		queryForm[0].classList.add("height");
-// 		queryForm[1].style.display = "none";
-// 	} else if (selectEl >= 9) {
-// 		queryForm[0].classList.remove("height");
-// 		queryForm[1].classList.add("height");
-// 	}
-// 	reset.style.display = "block";
-// 	const ar = [];
-// 	for (let key in input) {
-// 		ar.push(input[key]);
-// 	}
-// 	// console.log(input);
-// 	const index = [parseInt(selectEl) - 1];
-
-// 	const spliced = ar.splice(index, 1);
-// 	// spliced.classList.add("scale");
-// 	spliced[0].classList.add("scale");
-// 	for (let key in ar) {
-// 		input[key].disabled = true;
-// 		ar[key].classList.add("gray");
-// 		// console.log(ar[key].classList);
-// 	}
-// 	input = ar;
-// }
-
-// form.addEventListener("submit", (e) => onSubmit(e));
-
-// if (i < 1)
 for (let key in input)
 	input[key].addEventListener("click", (event) => {
-		// reset.style.display = "block";
-		i++;
 		event.preventDefault();
 		selectEl = event.target.value;
 		const index = [parseInt(selectEl) - 1];
@@ -65,17 +71,13 @@ for (let key in input)
 		for (let key in input) {
 			ar.push(input[key]);
 		}
-		for (let key in ar) {
-			// ar[key].classList.remove("scale");
-			// console.log(ar[key].classList);
-		}
-		// console.log(input);
 
 		const spliced = ar.splice(index, 1);
-		spliced[0].classList.add("scale");
+		spliced[0].style.transform = "scale(1.2)";
 		spliced[0].classList.remove("gray");
-		for (let key in ar) {
-			ar[key].classList.remove("scale");
-			ar[key].classList.add("gray");
+		for (let inde in ar) {
+			ar[inde].style.transform = "scale(1)";
+			ar[inde].style.removeProperty("transform");
+			ar[inde].classList.add("gray");
 		}
 	});
